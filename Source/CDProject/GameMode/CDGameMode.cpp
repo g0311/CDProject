46a5225d51ac00cbaa4ff20c8fce3f38fc560ebd
g0311 <gsc0815@naver.com>
2025-03-19 00:03:57 +0900

    [FIX] Include Path

--- Source/CDProject/GameMode/CDGameMode.cpp
+++ Source/CDProject/GameMode/CDGameMode.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "GameMode/CDGameMode.h"
+#include "CDGameMode.h"
 
--- Source/CDProject/Weapon/ProjectileWeapon.cpp
+++ Source/CDProject/Weapon/ProjectileWeapon.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Weapon/ProjectileWeapon.h"
+#include "ProjectileWeapon.h"
 
 // Sets default values
 AProjectileWeapon::AProjectileWeapon()
