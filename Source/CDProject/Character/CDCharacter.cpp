7b840140993b0c89c8879d7edfd50e06623f7f3f
g0311 <gsc0815@naver.com>
2025-04-28 22:55:48 +0900

    [FIX] Tab KDOverlay Input

--- Source/CDProject/Character/CDCharacter.cpp
+++ Source/CDProject/Character/CDCharacter.cpp
@@ -158,8 +158,8 @@ void ACDCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 		enhancedInputComponent->BindAction(_dropWeaponAction, ETriggerEvent::Completed, this, &ACDCharacter::RequestDropWeapon);
 		enhancedInputComponent->BindAction(_interactAction, ETriggerEvent::Started, this, &ACDCharacter::RequestInteractStart);
 		enhancedInputComponent->BindAction(_interactAction, ETriggerEvent::Completed, this, &ACDCharacter::RequestInteractEnd);
-		// enhancedInputComponent->BindAction(_tabAction, ETriggerEvent::Started, this, &ACDCharacter::RequestInteractEnd);
-		// enhancedInputComponent->BindAction(_tabAction, ETriggerEvent::Completed, this, &ACDCharacter::RequestInteractEnd);
+		enhancedInputComponent->BindAction(_tabAction, ETriggerEvent::Started, this, &ACDCharacter::TabStart);
+		enhancedInputComponent->BindAction(_tabAction, ETriggerEvent::Completed, this, &ACDCharacter::TabEnd);
 	}
 }
 
@@ -592,6 +592,22 @@ void ACDCharacter::RequestInteractEnd()
 	if (!_combat)
 		return;
 	_combat->RequestInteractEnd();
+}
+
+void ACDCharacter::TabStart()
+{
+	ACDPlayerController* pc = Cast<ACDPlayerController>(GetController());	
+	if(!IsValid(pc))
+		return;
+	pc->ShowKDOverlay(true);
+}
+
+void ACDCharacter::TabEnd()
+{
+	ACDPlayerController* pc = Cast<ACDPlayerController>(GetController());	
+	if(!IsValid(pc))
+		return;
+	pc->ShowKDOverlay(false);
 }
 
 //Always Called By Server
